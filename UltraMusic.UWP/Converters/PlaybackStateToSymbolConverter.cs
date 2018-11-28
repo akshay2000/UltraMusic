039a127dddf0553d2241cd78ea2168b4e78ab7b7
Akshay Zade <akshay2000@hotmail.com>
2018-11-28 23:38:27 +0530

    UI enhancements and converters

---
+++ UltraMusic.UWP/Converters/PlaybackStateToSymbolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltraMusic.Portable.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace UltraMusic.UWP.Converters
+{
+    public class PlaybackStateToSymbolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            PlayerState state = (PlayerState)value;
+            switch (state)
+            {
+                default:
+                case PlayerState.Idle:
+                case PlayerState.Paused:
+                    return new SymbolIcon(Symbol.Play);
+                case PlayerState.Playing:
+                    return new SymbolIcon(Symbol.Pause);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UltraMusic.UWP/Converters/StringToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UltraMusic.UWP.Converters
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string left = value.ToString();
+            string[] rights = parameter.ToString().Split('|');
+            return rights.Contains(left);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
