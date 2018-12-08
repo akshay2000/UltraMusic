c1d463e5da042ecdea388e7e369327ceaf99732a
SanketZade <sanketzade@outlook.com>
2018-12-08 11:53:40 +0530

    Added JS for GoogleMusic

--- UltraMusic.UWP/Assets/ProvidersSpec/GoogleMusic/Functions.js
+++ UltraMusic.UWP/Assets/ProvidersSpec/GoogleMusic/Functions.js
@@ -1,20 +1,5 @@
 ﻿function observePlaybackState() {
-    if (document.isObservingPlaybackState)
-        return;
-
-    var targetNode = document.getElementsByClassName("material-player-middle");
-    console.log(targetNode);
-    if (!targetNode)
-        return;
-
-    var observerConfig = { attributes: true, childList: true, subtree: true };
-
-    var playbackStateChanged = function (mutations, observer) {
-        window.external.notify("PlaybackStateChanged");
-    };
-    var playbackStateObserver = new MutationObserver(playbackStateChanged);
-    playbackStateObserver.observe(targetNode, observerConfig);
-    document.isObservingPlaybackState = true;
+    
 }
 
 function getPlayerState() {
